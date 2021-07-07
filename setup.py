1170fb03b9e1c6c3642255d67d098a6a1cd53be0
Yasser Zairi <yzairi@bioscript.ca>
2021-07-07 13:30:56 -0300

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine 0 package",
+    author="yz",
+    packages=find_packages(),
+    licence="MIT"
+)
